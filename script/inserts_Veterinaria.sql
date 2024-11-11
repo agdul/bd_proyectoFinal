1ab454256f9a55e1b363d0950608a7fc73fd7075
agdul <abdul.trc@gmail.com>
2024-11-11 17:02:45 -0300

    insert 1millon

--- script/inserts_Veterinaria.sql
+++ script/inserts_Veterinaria.sql
@@ -678,6 +678,7 @@ INSERT INTO Especialidad (nombre_especialidad) SELECT nombre_especialidad FROM E
 
 INSERT INTO Laboratorio (nombre_lab) SELECT nombre_lab FROM Laboratorio;
 
+--Ejecutar varias veces para tener un lote grande de mascotas 
 INSERT INTO Mascota (nombre_mascota, fecha_nacimiento, peso_mascota, condicion_mascota, id_dueno, id_raza)
 SELECT nombre_mascota, fecha_nacimiento, peso_mascota, condicion_mascota, id_dueno, id_raza
 FROM Mascota;
@@ -686,7 +687,8 @@ INSERT INTO Medicamento (nombre_comercial, monodroga_medic, presentacion_medic, 
 SELECT nombre_comercial, monodroga_medic, presentacion_medic, id_laboratorio
 FROM Medicamento;
 
---INICIO DE LA FUNCION DUPLICAR VET
+--INICIO DE LA FUNCION DUPLICAR VET 
+-- Ejecutar varias veces para poder llegar al millon de insersiones en cita medica 
 WITH CTE AS (
     SELECT  nro_licProfesional + ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS nuevo_nro_licProfesional,
             nombre_profesional,
@@ -745,3 +747,48 @@ WHERE NOT EXISTS (
 
 -- Eliminar la tabla temporal
 DROP TABLE #TempTratamientoMedicamento;
+
+
+-- INSERT 1 millon de datos Cita medica tabla de mayor ocurrencia 
+
+-- Creamos una nueva tabla
+CREATE TABLE CitasMedicaNew
+(
+  id_citaMedica INT IDENTITY(1,1),
+  fecha_citaMedica DATE NOT NULL DEFAULT GETDATE(),
+  observaciones_citaMedica VARCHAR(70) NOT NULL,
+  usuario VARCHAR(50) NOT NULL DEFAULT SYSTEM_USER,
+  motivo_visita VARCHAR(50) NOT NULL,
+  id_mascota INT NOT NULL,
+  id_veterinario INT NOT NULL,
+  CONSTRAINT PK_CitasMedicaNew_id PRIMARY KEY (id_citaMedica),
+  CONSTRAINT FK_CitasMedicaNew_id_mascota FOREIGN KEY (id_mascota) REFERENCES Mascota(id_mascota),
+  CONSTRAINT FK_CitasMedicaNew_id_veterinario FOREIGN KEY (id_veterinario) REFERENCES Veterinario(id_veterinario)
+);
+
+-- Carga masiva de datos 
+INSERT INTO CitasMedicaNew (fecha_citaMedica, observaciones_citaMedica, usuario, motivo_visita, id_mascota, id_veterinario)
+SELECT TOP 1000000
+  DATEADD(day, CAST(5000 * RAND(CHECKSUM(NEWID())) + 1 AS INT), '2010-01-01') AS fecha_citaMedica,
+  'Observación generada' AS observaciones_citaMedica,
+  SYSTEM_USER AS usuario,
+  'Consulta general' AS motivo_visita,
+  m.id_mascota AS id_mascota,  -- Selección de id_mascota válidos de la tabla Mascota
+  v.id_veterinario AS id_veterinario  -- Selección de id_veterinario válidos de la tabla Veterinario
+FROM Mascota m
+JOIN Veterinario v ON 1 = 1;  -- Este JOIN asegura que tomas todos los id_veterinario disponibles
+
+
+
+
+-- Contador de registro total
+SELECT 
+t.name
+ AS tabla, SUM(p.rows) AS row_count
+FROM sys.tables AS t
+INNER JOIN sys.partitions AS p ON t.object_id = p.object_id
+WHERE p.index_id IN (0, 1)  -- 0 para heaps (tablas sin índice clustered) y 1 para índices clustered
+GROUP BY 
+t.name
+
+ORDER BY row_count DESC; 

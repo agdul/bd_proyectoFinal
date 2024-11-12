d9e2f7af775fa17d54dfea13f5574f8af4ddf278
Pabli1314 <124464585+Pabli1314@users.noreply.github.com>
2024-11-12 17:03:27 -0300

    Tema 3

--- script/tema03_optimizacionConsultasAtravezdeIndices/insert_optimizacionConsultas.sql
+++ script/tema03_optimizacionConsultasAtravezdeIndices/insert_optimizacionConsultas.sql
@@ -22,3 +22,105 @@ CREATE COLUMNSTORE INDEX IX_Columnar_CitasMedicaNew ON CitasMedica (fecha_citaMe
 --Elimina el indice IDX_gastoNEW de la tabla gastoNew
 DROP INDEX IX_Columnar_CitasMedicaNew ON CitasMedicaNew; 
 -------------------------------------------------------------------------------------------------
+
+-- INSERT 1 millon de datos Cita medica tabla de mayor ocurrencia 
+-- Duplicar Mascotas ya creadas 
+
+--Ejecutar varias veces para tener un lote grande de mascotas 
+INSERT INTO Mascota (nombre_mascota, fecha_nacimiento, peso_mascota, condicion_mascota, id_dueno, id_raza)
+SELECT nombre_mascota, fecha_nacimiento, peso_mascota, condicion_mascota, id_dueno, id_raza
+FROM Mascota;
+
+-- Ejecutar varias veces para poder llegar al millon de insersiones en cita medica 
+WITH CTE AS (
+    SELECT  nro_licProfesional + ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS nuevo_nro_licProfesional,
+            nombre_profesional,
+            hora_entrada,
+            hora_salida,
+            id_especialidad
+    FROM Veterinario
+)
+INSERT INTO Veterinario (nro_licProfesional, nombre_profesional, hora_entrada, hora_salida, id_especialidad)
+SELECT  nuevo_nro_licProfesional,
+        nombre_profesional,
+        hora_entrada,
+        hora_salida,
+        id_especialidad
+FROM CTE
+WHERE NOT EXISTS (
+    SELECT 1 
+    FROM Veterinario v 
+    WHERE v.nro_licProfesional = CTE.nuevo_nro_licProfesional
+);
+
+--FIN DE LA FUNCION DUPLICAR VET
+
+
+
+
+
+
+
+
+
+
+-- Creamos una nueva tabla
+CREATE TABLE CitasMedicaNew
+(
+  id_citaMedica INT IDENTITY(1,1),
+  fecha_citaMedica DATE NOT NULL DEFAULT GETDATE(),
+  observaciones_citaMedica VARCHAR(70) NOT NULL,
+  usuario VARCHAR(50) NOT NULL DEFAULT SYSTEM_USER,
+  motivo_visita VARCHAR(50) NOT NULL,
+  id_mascota INT NOT NULL,
+  id_veterinario INT NOT NULL,
+  CONSTRAINT PK_CitasMedicaNew_id PRIMARY KEY (id_citaMedica),
+  CONSTRAINT FK_CitasMedicaNew_id_mascota FOREIGN KEY (id_mascota) REFERENCES Mascota(id_mascota),
+  CONSTRAINT FK_CitasMedicaNew_id_veterinario FOREIGN KEY (id_veterinario) REFERENCES Veterinario(id_veterinario)
+);
+
+
+select count(*) from CitasMedicaNew
+
+
+
+
+
+-- Carga masiva de datos en la tabla CitaMedicaNew
+INSERT INTO CitasMedicaNew (fecha_citaMedica, observaciones_citaMedica, usuario, motivo_visita, id_mascota, id_veterinario)
+SELECT TOP 1000000
+  DATEADD(day, CAST(5000 * RAND(CHECKSUM(NEWID())) + 1 AS INT), '2010-01-01') AS fecha_citaMedica,
+  'Observación generada' AS observaciones_citaMedica,
+  SYSTEM_USER AS usuario,
+  'Consulta general' AS motivo_visita,
+  m.id_mascota AS id_mascota,  -- Selección de id_mascota válidos de la tabla Mascota
+  v.id_veterinario AS id_veterinario  -- Selección de id_veterinario válidos de la tabla Veterinario
+FROM Mascota m
+JOIN Veterinario v ON 1 = 1;  -- Este JOIN asegura que tomas todos los id_veterinario disponibles
+
+
+
+-- Carga masiva de datos en la tabla CitaMedica
+INSERT INTO CitasMedica (fecha_citaMedica, observaciones_citaMedica, usuario, motivo_visita, id_mascota, id_veterinario)
+SELECT TOP 1000000
+  DATEADD(day, CAST(5000 * RAND(CHECKSUM(NEWID())) + 1 AS INT), '2010-01-01') AS fecha_citaMedica,
+  'Observación generada' AS observaciones_citaMedica,
+  SYSTEM_USER AS usuario,
+  'Consulta general' AS motivo_visita,
+  m.id_mascota AS id_mascota,  -- Selección de id_mascota válidos de la tabla Mascota
+  v.id_veterinario AS id_veterinario  -- Selección de id_veterinario válidos de la tabla Veterinario
+FROM Mascota m
+JOIN Veterinario v ON 1 = 1;  -- Este JOIN asegura que tomas todos los id_veterinario disponibles
+
+
+-- Contador de registro total
+SELECT 
+t.name
+ AS tabla, SUM(p.rows) AS row_count
+FROM sys.tables AS t
+INNER JOIN sys.partitions AS p ON t.object_id = p.object_id
+WHERE p.index_id IN (0, 1)  -- 0 para heaps (tablas sin índice clustered) y 1 para índices clustered
+GROUP BY 
+t.name
+
+ORDER BY row_count DESC; 
--- script/tema03_optimizacionConsultasAtravezdeIndices/optimizacionConsultas.sql
+++ script/tema03_optimizacionConsultasAtravezdeIndices/optimizacionConsultas.sql
@@ -15,5 +15,99 @@ WHERE fecha_citaMedica BETWEEN '2012-01-01' AND '2024-12-31';
 -- SET STATISTICS TIME OFF;
 -- SET STATISTICS PROFILE OFF;
 
+-- el siguiente script es para saber si un indice existe
+SELECT name 
+FROM sys.indexes 
+WHERE object_id = OBJECT_ID('CitasMedicaNew') 
+  AND is_primary_key = 0 
+  AND is_unique = 0; 
+
+--------------------------------------------------------------------------------------------------
+-- Ejecutar la consulta sin indices  
+-- Activar el plan de ejecución
+SET STATISTICS TIME ON;
+SET STATISTICS IO ON;
+
+-- Repetir la consulta por período sin indices
+SELECT *
+FROM CitasMedica
+WHERE fecha_citaMedica BETWEEN '2024-01-01' AND '2024-12-31';
+
+-- Desactivar el plan de ejecución
+SET STATISTICS TIME OFF;
+SET STATISTICS IO OFF;
+
+--------------------------------------------------------------------------------------------------
+
+--------Para crear indices agrupados debimos deshabilitar restricciones.
+-- Deshabilitar las restricciones de clave foránea
+ALTER TABLE CitasMedica NOCHECK CONSTRAINT FK_CitasMedica_id_mascota;
+ALTER TABLE CitasMedica NOCHECK CONSTRAINT FK_CitasMedica_id_veterinario;
+
+--Eliminar la clave foranea FK_tratamiento_id_cita
+ALTER TABLE tratamiento DROP CONSTRAINT FK_tratamiento_id_cita;
+
+-- Eliminar la restriccion de clave primaria 
+ALTER TABLE CitasMedica DROP CONSTRAINT PK_CitasMedica_id;
+
+--Se creo un indice agrupado con la columna fecha_citaMedica.
+CREATE CLUSTERED INDEX IX_CitasMedica_FechaCitaMedica 
+ON CitasMedica(fecha_citaMedica);
+
+ --Restaurar las restricciones de clave foránea
+ALTER TABLE CitasMedica WITH CHECK CHECK CONSTRAINT FK_CitasMedica_id_mascota;
+ALTER TABLE CitasMedica WITH CHECK CHECK CONSTRAINT FK_CitasMedica_id_veterinario;
 
 
+-- Activar el plan de ejecución
+SET STATISTICS TIME ON;
+SET STATISTICS IO ON;
+
+-- Repetir la consulta por período a traves de indices
+SELECT *
+FROM CitasMedica
+WHERE fecha_citaMedica BETWEEN '2024-01-01' AND '2024-12-31';
+
+-- Desactivar el plan de ejecución
+SET STATISTICS TIME OFF;
+SET STATISTICS IO OFF;
+
+--Eliminar indice
+DROP INDEX IX_CitasMedica_FechaCitaMedica ON CitasMedica;
+
+--------------------------------------------------------------------------------------------------
+
+/*
+Definir otro índice agrupado sobre la columna fecha pero que además incluya las columnas 
+seleccionadas y repetir la consulta anterior. Registrar el plan de ejecución utilizado 
+por el motor y los tiempos de respuesta.
+*/
+
+-- Deshabilitar las restricciones de clave foránea
+ALTER TABLE CitasMedica NOCHECK CONSTRAINT FK_CitasMedica_id_mascota;
+ALTER TABLE CitasMedica NOCHECK CONSTRAINT FK_CitasMedica_id_veterinario;
+
+--Eliminar la clave foranea FK_tratamiento_id_cita
+ALTER TABLE tratamiento DROP CONSTRAINT FK_tratamiento_id_cita;
+
+-- Eliminar el índice agrupado actual
+ALTER TABLE CitasMedica DROP CONSTRAINT PK_CitasMedica_id;
+
+--Crear el índice agrupado en fecha_citaMedica:
+CREATE CLUSTERED INDEX IX_CitasMedica_FechaCitaMedica 
+ON CitasMedica(id_mascota, id_veterinario, fecha_citaMedica);
+
+-- Activar el plan de ejecución
+SET STATISTICS TIME ON;
+SET STATISTICS IO ON;
+
+--Ejecutar nuevamente la consulta
+SELECT *
+FROM CitasMedica
+WHERE fecha_citaMedica BETWEEN '2024-01-01' AND '2024-12-31';
+
+-- Desactivar el plan de ejecución
+SET STATISTICS TIME OFF;
+SET STATISTICS IO OFF;
+
+DROP INDEX IX_CitasMedica_FechaCitaMedica ON CitasMedica;
